1e80a106b8610e4fffb9b144b577b4b63f8bb534
edwin363 <fedwin363@gmail.com>
2020-05-01 01:59:16 -0600

    agregando actions

--- src/redux/actions/loginActions.js
+++ src/redux/actions/loginActions.js
@@ -5,3 +5,42 @@ export const LOGIN_SUCCESS = 'LOGIN_REQUEST';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 
 //Actions
+export const loginRequest = () => {
+    return{
+        type: LOGIN_REQUEST
+    }
+}
+
+export const loginSuccess = (user, license) => {
+    return{
+        type: LOGIN_SUCCESS,
+        payload: {
+            user,
+            license
+        }
+    }
+}
+
+export const loginFailure = (error) => {
+    return{
+        type: LOGIN_FAILURE,
+        payload: error
+    }
+}
+
+const virtualLogin = (user) => {
+    return (dispatch) => {
+        dispatch(loginRequest());
+        Axios.post(``, {
+            name: "",
+            license: ""
+        }).then(response => {
+            dispatch(loginRequest([response.data]));
+        })
+        .catch(error => {
+            dispatch(loginFailure('Usuario y/o contraseña incorrectos'))
+        })
+    }
+}
+
+export default virtualLogin;
